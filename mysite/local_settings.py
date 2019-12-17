3ccf30fb06d21a74317b3edbccc8fa769ce1e144
Jonathan Garza <jonathan.garza@rigup.com>
2019-12-17 12:13:44 -0600

    First commit

---
+++ mysite/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'poll',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': 3001
+    }
+}
